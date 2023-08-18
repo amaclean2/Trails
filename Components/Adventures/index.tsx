59edbc35d69cf94e4cc4ac8606618c0d0a0c6484
Andrew Maclean <andrewmaclean@kingbook.lan>
2023-08-18 11:46:44 -0700

    Created a navigation for the app Now just fill in the components

---
+++ Components/Adventures/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AdventureView = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hi, there</Text>
+    </View>
+  );
+};
+
+export default AdventureView;
